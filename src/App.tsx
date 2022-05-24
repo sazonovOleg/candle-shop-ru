1483797fa6e4c2b705ee56d975a46610f210bbc7
olegaz209 <o.sazonov@propellerads.net>
2022-05-24 19:04:31 +0300

    add new style & components

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import {AppBody} from './App.module.scss';
 import {Header} from "./components/Header";
 import {BannerPage} from "./components/BannerPage";
+import {PreviewShopProducts} from "./components/PreviewShopProducts";
 
 function App() {
     return (
         <div className={AppBody}>
             <Header/>
             <BannerPage/>
+            <PreviewShopProducts/>
         </div>
     );
 }
---
+++ src/components/PreviewShopProducts/type.ts
@@ -0,0 +1,7 @@
+export type PreviewShopProductsType = {
+    image: string,
+    title: string,
+    description: string,
+    price: number,
+    btnText:string
+}
